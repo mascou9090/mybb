3e8c732a3e4066417f74497d498a1282d20c8bdb
Marcondes Ferreira <bshoopkss@gmail.com>
2024-08-25 01:18:50 -0300

    First commit

---
+++ app/components/eyes/page.tsx
@@ -0,0 +1,8 @@
+//import Eyes from "../../../assets/yourEyes.png"
+export default function Eyes() {
+  return (
+    <main className="flex justify-center h-[600px]">
+      <h3>Eyes</h3>
+    </main>
+  );
+}
